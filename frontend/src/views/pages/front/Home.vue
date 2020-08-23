67c7fb147ed3034dadbfa46f8da4cd5c91b9d2da
imamsudrajat04 <imamsudrajat707@gmail.com>
2020-08-24 00:34:23 +0700

    template login

---
+++ frontend/src/views/pages/front/Home.vue
@@ -0,0 +1,13 @@
+<template>
+    <FrontLayout>
+    </FrontLayout>
+</template>
+<script>
+import FrontLayout from '@/views/layouts/FrontLayout';
+export default {
+    name:'Home',
+    components:{
+        FrontLayout
+    }
+}
+</script>
---
+++ frontend/src/views/pages/front/Login.vue
@@ -0,0 +1,38 @@
+<template>
+    <FrontLayout>
+        <v-container fluid>
+            <v-row align="center">
+                <v-col cols="12">
+                    <v-form ref="form">
+                        <v-text-field 
+                            v-model="email" 
+                            :rules="emailRules" 
+                            label="E-mail" 
+                            required>
+                        </v-text-field>
+                        <v-text-field
+                            v-model="password"
+                            :append-icon="show1 ? 'mdi-eye' : 'mdi-eye-off'"
+                            :type="show1 ? 'text' : 'password'"
+                            name="input-10-1"
+                            label="Password"
+                            hint="At least 8 characters"
+                            counter
+                            @click:append="show1 = !show1"
+                        >
+                        </v-text-field>
+                    </v-form>
+                </v-col>
+            </v-row>
+        </v-container>
+    </FrontLayout>
+</template>
+<script>
+import FrontLayout from '@/views/layouts/FrontLayout';
+export default {
+    name:'Login',
+    components: {
+        FrontLayout
+    }
+}
+</script>
