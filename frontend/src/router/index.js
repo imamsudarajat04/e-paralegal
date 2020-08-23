581436420cc206045e9e03603480f9466ea9f5ac
imamsudrajat04 <imamsudrajat707@gmail.com>
2020-08-23 22:07:28 +0700

    Halaman login

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,29 +1,32 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
 
 Vue.use(VueRouter)
 
-  const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
-]
+	const routes = [
+		{
+			path: '/',
+			name: 'FrontDashboard',
+			meta:{
+				title: "DASHBOARD"
+			},
+			component: () => import('../views/pages/front/Home.vue')
+		},
+
+		{
+			path: '/login',
+			name: 'FrontLogin',
+			meta:{
+				title: "LOGIN"
+			},
+			component: () => import('../views/pages/front/Login.vue')
+		},
+	]
 
 const router = new VueRouter({
-  mode: 'history',
-  base: process.env.BASE_URL,
-  routes
+	mode: 'history',
+	base: process.env.BASE_URL,
+	routes
 })
 
 export default router
