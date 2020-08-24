c0dc9fb2df961e2d216908b2046453a45fb5aa2e
imamsudrajat04 <imamsudrajat707@gmail.com>
2020-08-24 23:26:49 +0700

    'FormLogin'

--- frontend/src/views/pages/front/Login.vue
+++ frontend/src/views/pages/front/Login.vue
@@ -3,56 +3,31 @@
         <v-container fluid>
             <v-row justify="center">
                 <v-col cols="12" sm="10" md="8" lg="6">
-                    <v-card ref="form">
-                        <v-card-text class="my-4 text-center title">Halaman Login</v-card-text>
-                            <v-card-text>
-                                <v-text-field
-                                    ref="email"
-                                    v-model="email"
-                                    :rules="[() => !!email || 'This field is required']"
-                                    :error-messages="errorMessage"
-                                    label="E-mail"
-                                    required
-                                >
-                                </v-text-field>
-                                <v-text-field
-                                    v-model="password"
-                                    :append-icon="show1 ? 'mdi-eye' : 'mdi-eye-off'"
-                                    :type="show1 ? 'text' : 'password'"
-                                    name="input-10-1"
-                                    label="Password"
-                                    hint="At least 8 characters"
-                                    counter
-                                    @click:append="show1 = !show1"
-                                >
-                                </v-text-field> 
-                            </v-card-text>
-                            <v-divider class="mt-12"></v-divider>
-                                <v-card-actions>
-                                    <v-btn text>Cancel</v-btn>
+                    <v-form ref="frmLogin" lazy-validation>
+                        <v-card>
+                            <v-card-text class="my-4 text-center title">Halaman Login</v-card-text>
+                                <v-card-text>
+                                    <v-text-field 
+                                        v-model="formlogin.username"
+                                        label="Username" 
+                                        :rules="rule_username"
+                                    >  
+                                    </v-text-field>
+                                    <v-text-field
+                                        v-model="formlogin.password"
+                                        label="Password"
+                                        type="password"
+                                        :rules="rule_password"
+                                    >
+                                    </v-text-field> 
+                                </v-card-text>
+                                <v-divider class="mt-12"></v-divider>
+                                    <v-card-actions> 
                                         <v-spacer></v-spacer>
-                                            <v-slide-x-reverse-transition>
-                                                <v-tooltip
-                                                    v-if="formHasErrors"
-                                                    left
-                                                >
-                                                    <template v-slot:activator="{ on, attrs }">
-                                                        <v-btn
-                                                            icon
-                                                            class="my-0"
-                                                            v-bind="attrs"
-                                                            @click="resetForm"
-                                                            v-on="on"
-                                                        >
-                                                            <v-icon>mdi-refresh</v-icon>
-                                                        </v-btn>
-                                                    </template>
-                                                    <span>Refresh form</span>
-                                                </v-tooltip>
-                                            </v-slide-x-reverse-transition>
-                                        <v-btn color="primary" text @click="submit">Login</v-btn>
-                                </v-card-actions>
-                        </v-card>
+                                        <v-btn color="primary" text @click="doLogin">Login</v-btn>
+                                    </v-card-actions>
+                            </v-card>
+                    </v-form>
                 </v-col>
             </v-row>
         </v-container>
@@ -62,6 +37,29 @@
 import FrontLayout from '@/views/layouts/FrontLayout';
 export default {
     name:'Login',
+    data(){
+        return {
+            formlogin: {
+                username:'',
+                password:''
+            },
+            rule_username:[
+                value => !!value || "Kolom Username mohon untuk diisi !!!"
+            ],
+            rule_password:[
+                value => !!value || "Kolom Password mohon untuk diisi !!!"
+            ]
+        }
+    },
+    methods: {
+        doLogin: async function()
+        {
+            if(this.$refs.frmLogin.validate())
+            {
+                console.log('test');
+            }
+        }
+    },
     components: {
         FrontLayout
     }
